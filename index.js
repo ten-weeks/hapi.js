8d4f9f5c0232975394175a086417035b5d91749f
shahenaz Monia <shahy.m.93@gmail.com>
2017-03-13 09:44:10 +0200

    update image

--- index.js
+++ index.js
@@ -1,111 +1,92 @@
-var hapi = require ('hapi');
+var hapi = require('hapi');
 var server = new hapi.Server();
-var path = require ('path');
-var pg = require ('pg');
-
-server.connection ({
-  host: 'localhost',
-  port : 8080,
-
-});
-
-
-var config ={
-  user : 'postgres',
-  database : 'hapi',
-  password : '1121993',
-  host : 'localhost'
+var path = require('path');
+var pg = require('pg');
+var id = 0;
+var config = {
+    user: 'postgres',
+    database: 'hapi',
+    password: '1121993',
+    host: 'localhost'
 }
-
 var client = new pg.Client(config);
-
-// server.register( require ('inert') , function (err) {
-//   if(err){
-//     throw err;
-//   }
-//   server.route({
-//     method : 'GET',
-//     path : '/facebook.jpg',
-//     handler : function (request,reply){
-//       reply.file('facebook.jpg');
-//     }
-//   })
-//
-// });
-
 client.connect();
-server.register( require ('vision') , function (err) {
-  if(err){
-    throw err;
-  }
 
+server.connection({
+    host: 'localhost',
+    port: 8080,
 
-// server.route({
-//   method : 'GET',
-//   path : '/{userName}',
-//   handler : function (request,reply){
-//     var name = request.params.userName
-//     reply.view('index', {userName : name});
-//   }
-//
-// });
+});
 
-server.route({
-  method : 'GET',
-  path : '/',
-  handler : function (request,reply){
-    client.query('SELECT * FROM people', function(err, result){
-      console.log(result.rows[0].name);
-      if(err){
-      throw err;
+server.register(require('inert'), function(err) {
+    if (err) {
+        throw err;
     }
-      reply('my name is ' + result.rows[0].name);
+    server.route({
+        method: 'GET',
+        path: '/hello.jpg',
+        handler: function(request, reply) {
+            reply.file('hello.jpg');
+        }
     })
-  }
 
 });
 
-});
+server.register(require('vision'), function(err) {
+    if (err) {
+        throw err;
+    }
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function(request, reply) {
+            reply.view('main');
+        }
+    })
+    server.route({
+        method: 'GET',
+        path: '/{userName}',
+        handler: function(request, reply) {
+            var name = request.params.userName;
+            id++;
+            client.query(`INSERT INTO people VALUES (${id},\'${name}\');`, function(err) {
+                if (err) {
+                    throw err;
+                }
 
-// server.views({
-//   engines : {
-//     html : require('handlebars')
-//   },
-//   relativeTo : __dirname,
-//     path : 'templates'
-//   });
-//
-// })
+            })
+            reply.view('index');
+        }
 
+    });
 
+    server.route({
+        method: 'GET',
+        path: '/{userName}/display',
+        handler: function(request, reply) {
+            client.query('SELECT * FROM people', function(err, result) {
+                if (err) {
+                    throw err;
+                }
+                var userName = request.params.userName
+                var user = result.rows.find(function (item) {
+                  return item.name === userName
+                })
+                reply.view('display', {name : user.name});
+            })
+        }
 
-// server.route({
-//   method : 'GET',
-//   path : '/home/{userName}',
-//   handler : function (request,reply){
-//     reply('help me ' + request.params.userName);
-//   }
-//
-// });
-// server.route({
-//   method : 'GET',
-//   path : '/facebook.jpg',
-//   handler : function (request,reply){
-//     reply.file('facebook.jpg');
-//   }
-// })
+    });
 
+    server.views({
+        engines: {
+            html: require('handlebars')
+        },
+        relativeTo: __dirname,
+        path: 'templates'
+    });
 
-//
-// server.route({
-//   method : 'GET',
-//   path : '/',
-//   handler : function (request,reply){
-//     reply("<img src = 'facebook.jpg' />");
-//   }
-// })
-
-// })
-server.start( function(){
-  console.log('listion to 3000', server.info.uri);
+})
+server.start(function() {
+    console.log('listion to 8080', server.info.uri);
 });
